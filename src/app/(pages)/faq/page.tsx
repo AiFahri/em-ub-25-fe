54e1e467a003edc2842355a2c3367bd86bbb51ab
Alfaril Dzaky Praptana <dzaky@Alfarils-MacBook-Air.local>
2025-06-27 18:59:56 +0700

    faq page

---
+++ src/app/(pages)/faq/page.tsx
@@ -0,0 +1,7 @@
+export default function FAQPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">FAQ Page</h1>
+    </div>
+  );
+}

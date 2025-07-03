bb981b9a0eba2aac87e1a63eaa2e09d0ba0e5ad6
wildan <wildanzhaf@gmail.com>
2025-07-04 04:59:50 +0700

    feat: create page for oprec and announcement

---
+++ src/app/(pages)/tolak/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tolak from '@/components/pengumuman/Tolak';
+
+const page = () => {
+  return (
+    <main>
+      <Tolak />
+      <div className="">TES</div>
+    </main>
+  );
+};
+
+export default page;

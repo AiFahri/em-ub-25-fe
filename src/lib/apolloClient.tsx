64a6ae0e2e44d067cb3bd418a38b8daadaa17f7a
wildan <wildanzhaf@gmail.com>
2025-07-04 04:54:25 +0700

    feat: add apollo client

--- src/lib/apolloClient.tsx
+++ src/lib/apolloClient.tsx
@@ -1,25 +1,18 @@
 // lib/apolloClient.ts
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider as ApolloHooksProvider,
-  HttpLink,
-  from,
-} from "@apollo/client";
-import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+'use client';
+
+import { ApolloClient, InMemoryCache, ApolloProvider as ApolloHooksProvider, HttpLink, from } from '@apollo/client';
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 
 const uploadLink = createUploadLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, // pastikan endpoint ini ada di .env
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
 }) as HttpLink;
 
-// Chain hanya uploadLink (tanpa auth & errorLink)
 const link = from([uploadLink]);
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
 });
 
-export const ApolloProvider = ({ children }: { children: React.ReactNode }) => (
-  <ApolloHooksProvider client={client}>{children}</ApolloHooksProvider>
-);
+export const ApolloProvider = ({ children }: { children: React.ReactNode }) => <ApolloHooksProvider client={client}>{children}</ApolloHooksProvider>;

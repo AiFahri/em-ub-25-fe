6da82b09860ae7cc51bc036b6a27f099ee32bfb1
wildan <wildanzhaf@gmail.com>
2025-07-04 05:00:44 +0700

    style: improve card responsiveness

--- src/components/landingpage/BeritaCard.tsx
+++ src/components/landingpage/BeritaCard.tsx
@@ -5,33 +5,33 @@ import beritaimg from '@/assets/landingpage/background/contohberita.png';
 
 type BeritaCardProps = NewsItem & { index: number };
 
-export default function BeritaCard({ title, date, description, image, index }: BeritaCardProps) {
+export default function BeritaCard({ title, date, description, imageUrl, index }: BeritaCardProps) {
   const isOrange = index % 2 === 0;
 
-  const showPlaceholder = !image?.trim();
+  const showPlaceholder = !imageUrl?.trim();
 
   return (
     <div className="rounded-2xl p-4 w-full h-full space-y-3 relative">
       <div className="relative">
-        <div className="w-full h-[330px] rounded-4xl relative overflow-hidden clip-concave-bottom-right">
+        <div className="w-full h-[200px] lg:h-[320px] rounded-4xl relative inverted-radius">
           {showPlaceholder ? (
             <div className="flex items-center justify-center w-full h-full bg-gray-100">
               <Image src="/Assets/icon/landingpage/placeholder.svg" alt={title} width={48} height={48} />
             </div>
           ) : (
-            <Image src={beritaimg} alt={title} fill className="object-cover" />
+            <Image src={`https://is3.cloudhost.id/em-ub-2025/${imageUrl.replace(/^\/+/, '')}`} alt={title} fill className="object-cover" />
           )}
         </div>
 
-        <div className={`absolute bottom-2 right-0 p-6 rounded-full z-20 ${isOrange ? 'bg-[#FF4900]' : 'bg-[#0049FF]'}`}>
-          <Image src={ArrowUpRight} alt="arrow" width={20} height={20} />
+        <div className={`absolute bottom-0 right-2 p-4 lg:p-6 rounded-full z-20 ${isOrange ? 'bg-[#FF4900]' : 'bg-[#0049FF]'}`}>
+          <Image src={ArrowUpRight} alt="arrow" className="w-[22px] md:w-[20px]" />
         </div>
       </div>
 
-      <div className="font-[NeueHaasDisplay]">
-        <h3 className="text-blue-900 font-medium text-3xl">{title}</h3>
-        <p className="text-xl font-medium text-[#001B5E]">{date}</p>
-        <p className="text-lg mt-2 font-medium text-[#001B5E]">{description}</p>
+      <div className="font-[NeueHaasDisplay] ">
+        <h3 className="text-[#002787] font-medium text-[clamp(2.8vw,3.8vw,40px)] truncate">{title}</h3>
+        <p className="text-[clamp(1.5vw,2.8vw,20px)] font-medium text-[#001B5E]">{date}</p>
+        <p className="text-[clamp(1.3vw,2.8vw,15px)] mt-2 font-medium text-[#001B5E] truncate">{description}</p>
       </div>
     </div>
   );

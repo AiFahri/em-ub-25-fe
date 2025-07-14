31361603f01cb4131958c75a326396cde746cd5f
wildan <wildanzhaf@gmail.com>
2025-07-15 01:39:18 +0700

    chore: remove unused motion import and clean up comments in Tolak page

--- src/components/pengumuman/Tolak.tsx
+++ src/components/pengumuman/Tolak.tsx
@@ -4,17 +4,14 @@ import tidaklolos from '@/assets/pengumuman/background/tidaklolos.svg';
 import bubblechat from '@/assets/pengumuman/icon/bubblechat.svg';
 import mori from '@/assets/pengumuman/icon/moritolak.svg';
 import o from '@/assets/pengumuman/icon/o.svg';
-import { motion } from 'framer-motion';
 import React from 'react';
 
 const Tolak = () => {
   return (
     <div className="relative w-full h-screen flex justify-center items-center px-4 overflow-hidden">
-      {/* Background image */}
       <Image src={tidaklolos} alt="background" fill className="absolute inset-0 -z-10 object-cover" priority />
 
       <div className="flex flex-col md:flex-row items-center justify-center md:gap-x-10 gap-y-5 xl:gap-x-20">
-        {/* Tulisan dan bubble */}
         <div className="flex flex-col items-center justify-center text-center">
           <div className="relative whitespace-nowrap font_bold text-[#FF4900] text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[7rem]">
             Belum Lolos!
@@ -29,8 +26,6 @@ const Tolak = () => {
             </div>
           </div>
         </div>
-
-        {/* Mori */}
         <Image src={mori} alt="mori" className="w-[120px] sm:w-[160px] md:w-[280px] xl:w-[400px] lg:-translate-x-20 lg:translate-y-0 md:translate-y-5 md:-translate-x-10 -translate-x-0" />
       </div>
     </div>

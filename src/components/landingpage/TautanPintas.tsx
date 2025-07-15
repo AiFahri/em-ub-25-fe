49f97fbb81db6ff1f46bdca7181ff8ec0235b30c
AiFahri <fahrinurilll@gmail.com>
2025-07-15 12:14:25 +0700

    refactor: standardize import statements and improve link handling in TautanPintas component

--- src/components/landingpage/TautanPintas.tsx
+++ src/components/landingpage/TautanPintas.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import Image from 'next/image';
-import arrowUpRight from '@/assets/landingpage/icons/arrow-up-right.svg';
-import arrowrightblue from '@/assets/landingpage/icons/arrow-right-blue.svg';
-import SkeletonTautanPintas from './SkeleteonTautanPintas';
-import arrowleftblue from '@/assets/landingpage/icons/arrow-left-blue.svg';
-import { GET_LANDING_PAGE_DATA } from '@/graphql/queries/getLandingPageData';
-import { useQuery } from '@apollo/client';
-import { useRouter } from 'next/navigation';
-import { motion } from 'framer-motion';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import Image from "next/image";
+import arrowUpRight from "@/assets/landingpage/icons/arrow-up-right.svg";
+import arrowrightblue from "@/assets/landingpage/icons/arrow-right-blue.svg";
+import SkeletonTautanPintas from "./SkeleteonTautanPintas";
+import arrowleftblue from "@/assets/landingpage/icons/arrow-left-blue.svg";
+import { GET_LANDING_PAGE_DATA } from "@/graphql/queries/getLandingPageData";
+import { useQuery } from "@apollo/client";
+import { motion } from "framer-motion";
+import { cubicBezier } from "framer-motion";
 
-const bgColors = ['#FF4900', '#0049FF', '#0538B9'];
-const overlayColors = ['#FF7C48', '#3A73FF', '#002787'];
+const bgColors = ["#FF4900", "#0049FF", "#0538B9"];
+const overlayColors = ["#FF7C48", "#3A73FF", "#002787"];
 
 export interface LinkItem {
   id: string;
@@ -27,7 +27,7 @@ export default function TautanPintas() {
   const { data, loading, error } = useQuery(GET_LANDING_PAGE_DATA);
 
   const tautanPintasData = data?.listLinks?.links ?? [];
-  const router = useRouter();
+  const customEase = cubicBezier(0.2, 0.8, 0.2, 1);
   const sectionVariant = {
     hidden: {
       opacity: 0,
@@ -42,7 +42,7 @@ export default function TautanPintas() {
       y: [100, -20, 10, 0],
       transition: {
         duration: 1.4,
-        ease: [0.2, 0.8, 0.2, 1],
+        ease: customEase,
       },
     },
   };
@@ -50,8 +50,16 @@ export default function TautanPintas() {
   if (loading) return <SkeletonTautanPintas />;
   if (error) return <p className="text-center">Gagal memuat tautan pintas.</p>;
   return (
-    <motion.section variants={sectionVariant} initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.3 }} className="py-12 px-4 md:px-10 w-full relative">
-      <h2 className="text-4xl md:text-6xl lg:text-8xl font-bold text-center text-[#0538B9] mb-10 md:mb-16">Tautan Pintas</h2>
+    <motion.section
+      variants={sectionVariant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.3 }}
+      className="py-12 px-4 md:px-10 w-full relative"
+    >
+      <h2 className="text-4xl md:text-6xl lg:text-8xl font-bold text-center text-[#0538B9] mb-10 md:mb-16">
+        Tautan Pintas
+      </h2>
 
       <div className="relative">
         <div className="hidden md:block absolute inset-0 z-30 pointer-events-none">
@@ -63,7 +71,11 @@ export default function TautanPintas() {
           transition-all duration-500 bg-white shadow-lg hover:shadow-xl 
           hover:scale-110 active:scale-95 z-30"
           >
-            <Image src={arrowleftblue} alt="Previous" className="w-[clamp(16px,2vw,28px)] h-auto" />
+            <Image
+              src={arrowleftblue}
+              alt="Previous"
+              className="w-[clamp(16px,2vw,28px)] h-auto"
+            />
           </button>
 
           <button
@@ -74,17 +86,23 @@ export default function TautanPintas() {
           transition-all duration-500 bg-white shadow-lg hover:shadow-xl 
           hover:scale-110 active:scale-95 z-30"
           >
-            <Image src={arrowrightblue} alt="Next" className="w-[clamp(16px,2vw,28px)] h-auto" />
+            <Image
+              src={arrowrightblue}
+              alt="Next"
+              className="w-[clamp(16px,2vw,28px)] h-auto"
+            />
           </button>
         </div>
 
-        <div className={`swiper multiple-slide-carousel swiper-container relative z-10`}>
+        <div
+          className={`swiper multiple-slide-carousel swiper-container relative z-10`}
+        >
           <Swiper
             modules={[Navigation]}
             loop={true}
             navigation={{
-              nextEl: '#slider-button-right',
-              prevEl: '#slider-button-left',
+              nextEl: "#slider-button-right",
+              prevEl: "#slider-button-left",
             }}
             spaceBetween={28}
             breakpoints={{
@@ -97,8 +115,19 @@ export default function TautanPintas() {
           >
             {tautanPintasData.map((links: LinkItem, i: number) => (
               <SwiperSlide key={links.id}>
-                <div onClick={() => router.push(`${links.url}`)} className="cursor-pointer relative h-[200px] lg:h-[320px] overflow-hidden rounded-[30px] group">
-                  <div className="absolute inset-0" style={{ backgroundColor: overlayColors[i % overlayColors.length], zIndex: 0 }} />
+                <a
+                  href={links.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="cursor-pointer relative h-[200px] lg:h-[320px] overflow-hidden rounded-[30px] group block"
+                >
+                  <div
+                    className="absolute inset-0"
+                    style={{
+                      backgroundColor: overlayColors[i % overlayColors.length],
+                      zIndex: 0,
+                    }}
+                  />
 
                   <div
                     className={`absolute inset-0 flex items-end p-8 z-10
@@ -118,11 +147,11 @@ export default function TautanPintas() {
                   </div>
 
                   <div className="absolute inset-0 top-10 md:top-10 lg:top-20 lg:right-20 flex items-end p-8 z-20 pointer-events-none">
-                    <a href={links.url} className="block text-[18px] md:text-2xl lg:text-4xl font-semibold text-white line-clamp-3">
+                    <span className="block text-[18px] md:text-2xl lg:text-4xl font-semibold text-white line-clamp-3">
                       {links.title}
-                    </a>
+                    </span>
                   </div>
-                </div>
+                </a>
               </SwiperSlide>
             ))}
           </Swiper>
